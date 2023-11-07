8603e17f4c53050ec24b54e5d79eff435b579cdd
Gabriel P <143112980+Gmprado78@users.noreply.github.com>
2023-11-07 10:47:45 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ function init(){
     var textbox = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = textbox.value;
     alert("Gabriel Prado: " + textbox.value);
-   }
+  }
 
-   button.addEventListener('click', showMeText);
-   }
+  button.addEventListener('click', showMeText);
+  }
 
 window.addEventListener('load', init);
